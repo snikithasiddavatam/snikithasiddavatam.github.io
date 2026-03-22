303daf110d37f20a387a9b26a1ecf1e0915095ac
snikithasiddavatam <snikithasiddavatam@gmail.com>
2026-03-21 23:56:25 -0500

    added Experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Experience from "./components/Experience"
 import Projects from "./components/Projects"
 import Contact from "./components/Contact"
 import Particles from "./components/Particles"
@@ -11,6 +12,7 @@ function App() {
       <div className="min-h-screen text-white overflow-auto">
         <Navbar />
         <Hero />
+        <Experience />
         <Projects />
         <Contact />
       </div>
