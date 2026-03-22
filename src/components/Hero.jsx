026b303d84bccea9acf2b10f66eecf389b542120
snikithasiddavatam <snikithasiddavatam@gmail.com>
2026-03-21 23:53:37 -0500

    more edits

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
 export default function Hero() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-4xl font-bold text-white">Hi, I'm Snikitha!</h1>
+            <p className="text-gray-400 mt-4 text-center">I'm passionate about high-performance computing, autonomous intelligence & competitive programming.</p>
         </div>
     )
 }
